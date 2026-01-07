76b82c103479a8a81007522d8b9e9ea235003242
Kaua Gouveia <kaua04gouveia@gmail.com>
2026-01-06 23:33:15 -0300

    feat: metodos para o teste 1

---
+++ js/script.js
@@ -0,0 +1,35 @@
+/* Estudo sobre JavaScript*/
+
+
+//console.log(document.getElementById('noticia').id)
+//let busca = document.getElementById('titulo');
+//console.table(busca);
+
+// let e = document.getElementById('titulo');
+// //console.log(e.classList);
+// console.log(e.innerHTML);
+// console.log(e.innerText);
+
+// ------------------------------------------------------/
+
+// let e = document.getElementsByClassName("outra-noticia");
+// console.log(e);
+// e[2].innerText = "Nova Notícia";
+
+
+// let e = document.getElementsByName("text_usuario");
+// e[0].value = "Joao"
+
+
+// let e = document.getElementsByTagName("div");
+// console.log(e);
+//----------------------------------------------------------/
+
+// let e = document.querySelector(".cor-texto-noticia");
+// console.log(e);
+
+// let a = document.querySelectorAll('p');
+// console.log(a);
+//-----------------------------------------------------------/
+
+    

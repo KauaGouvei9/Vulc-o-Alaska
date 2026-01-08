d3cc8932b637c665cbbbe020d5fa5eb1f2164e95
Kaua Gouveia <kaua04gouveia@gmail.com>
2026-01-08 16:17:16 -0300

    Descrição do querySelector and All

--- js/script.js
+++ js/script.js
@@ -32,4 +32,81 @@
 // console.log(a);
 //-----------------------------------------------------------/
 
-    
+// EXEMPLOS DE SELETORES E RESULTADOS
+
+// let el = null;
+
+// //universal selector - *
+// // -----------------------
+// // buscar o 1º elemento do documento
+// el = document.querySelector('*')
+
+// // buscar todos os elementos do documento
+// el = querySelectorAll('*')
+
+
+// // type selector - p / div / h1/...
+// //------------------------------------
+// //buscar o primeiro paragrafo do documento
+// el = document.querySelector('p')
+
+// //buscar todos os paragrafos do documento
+// el = document.querySelectorAll('p')
+
+// //class selector
+// //--------------------
+// //buscar o 1º elemento que contem a classe "texto" no atributo class
+// el.document.querySelector('.texto')
+
+// //buscar todos os elementos  que contem a classe "texto" no class
+// el.document.querySelectorAll('.texto')
+
+
+// // id selector - #
+// //----------------------------------
+// //buscar elemento que tem id indicado
+// el = document.querySelector('#texto')
+
+// //uma vez que o id é unico, nao faz sentido usar o querySelectorAll()
+
+// //attribute selector - [attribute]
+// //-----------------------------------
+
+// //todos os elementos que contem atributo NAME
+// el = document.querySelectorAll('[name]')
+
+// //todos os elementos paragrafos que contem atributo NAME 
+// el = document.querySelectorAll('p[name]')
+
+// //todos os elementos que contem atributo NAME e cujo o value seja TEXTO
+// el = document.querySelectorAll('[name="texto"]')
+
+// //todos os elementos que contem atributo class e cujo o value tem
+// //varias palavras separadas por espaço e uma delas é "letra"
+// el = document.querySelectorAll('[class~="letra"]') // class="texto letra cor"
+
+
+// //todos os elementos que contem atributo class e cujo o value começa
+// //por uma determinada palavra
+// el = document.querySelectorAll('[class^="color"]') // basta ter o color no inico que capturamos o elemento
+
+// //todos os elementos que contem atributo class e cujo o value que termina
+// //por uma expressão "size"
+// el = document.querySelectorAll('[class$="size"]') // basta ter o size no final que capturamos o elemento
+
+// // todos os elementos cujo atributo class tem a palavra TEXT pelo menos uma vez
+// el = document.querySelectorAll('[class*="text"]') //class="texto" ele ainda vai buscar o elemento
+
+// // podemos agrupar seletores - busca div, p e h1
+// el = document.querySelectorAll('div, p, h1')
+
+// //ou buscar os descendentes
+// el = document.querySelectorAll("div ul li")
+
+// // descentes diretos 
+// el = document.querySelectorAll("div > h3")
+
+// //entre varios outros
+
+// //resumo: todos os seletores sao validos no css 
+// // querySelector() ou queryselectorAll()

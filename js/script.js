3e9ed348513ed6b5e124227b6e78cd3d67570ee4
Kaua Gouveia <kaua04gouveia@gmail.com>
2026-01-12 23:10:18 -0300

    Atributos de elementos HTML

--- js/script.js
+++ js/script.js
@@ -159,3 +159,31 @@
 // } else {
 //     console.log('A classe container não existe.')
 // }
+
+//------------------------------------------------------------------------------------------------------------
+// MANIPULAR ATRIBUTOS DE ELEMENTOS HTML
+
+//let img = document.querySelector('img')
+//let link = document.querySelector('a')
+
+// -------------------------------
+// VER ATRIBUTO DE ELEMENTO
+// -------------------------------
+// console.log(img.getAttribute('src'))
+// console.log(link.getAttribute('href'))
+
+// -------------------------------
+// DEFINIR OU ALTERAR ATRIBUTO DE ELEMENTO
+// -------------------------------
+
+// img.setAttribute('src', 'images/image_2.jpg')
+// link.setAttribute('href', 'https://www.spacex.com/')
+// link.setAttribute('target', '_blank')
+// link.setAttribute('title', 'SpaceX')
+
+// -------------------------------
+// REMOVER ATRIBUTO DE ELEMENTO
+// -------------------------------
+
+// link.removeAttribute('href')
+// link.removeAttribute('title')

6fcc18abe8b0d3d1372d07b36dc58188886ff91a
Kaua Gouveia <kaua04gouveia@gmail.com>
2026-01-18 23:48:47 -0300

    Manipular eventos

--- js/script.js
+++ js/script.js
@@ -226,3 +226,26 @@
 // titulo.textContent = 'Título do elemento 4'
 // novo_div.appendChild(titulo)
 // document.body.appendChild(novo_div)
+
+
+//------------------------------------------------------------------------------------------------------------
+// MANIPULAR EVENTOS
+
+
+// function alterar_texto(){
+//     document.querySelector('h1').textContent = "Novo texto"
+// }
+
+
+// // não dá para adicionar mais do que um event handler a cada evento
+// let botao = document.querySelector('#botao')
+// botao.onclick = funcao1
+// botao.onclick = funcao2
+
+// function funcao1(){
+//     console.log('clique')
+// }
+
+// function funcao2(){
+//     document.querySelector('h1').textContent = "Novo texto1"
+// }
